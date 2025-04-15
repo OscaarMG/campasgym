88ffb59ea650bbf425e91723c2395c45cf23d209
OscaarMG <omartg08@estudiantes.unileon.es>
2025-04-15 20:44:22 +0200

    Login

--- src/vista/Login.py
+++ src/vista/Login.py
@@ -2,7 +2,7 @@ from PyQt6.QtWidgets import QApplication, QMainWindow
 from PyQt6 import uic
 
 # Cargar la interfaz generada desde el archivo .ui
-Form, Window = uic.loadUiType("./src/vista/Ui/VistaLogin.ui")
+Form, Window = uic.loadUiType("./src/vista/ui/VistaLogin.ui")
 
 class Login(QMainWindow, Form):
     def __init__(self):
