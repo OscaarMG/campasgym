608c4000a9d659cf9e6f5be055b2b3c412205e24
mrebot00 <147399784+mrebot00@users.noreply.github.com>
2025-05-31 00:23:21 +0200

    Add files via upload

---
+++ src/controlador/CoordinadorPrincipal.py
@@ -0,0 +1,80 @@
+from src.modelo.vo.LoginVO import LoginVO
+from src.vista.SocioWindow import SocioWindow
+from src.vista.EntrenadorWindow import EntrenadorWindow
+from src.vista.AdministrativoWindow import AdministrativoWindow
+from src.vista.SocioPremiumWindow import SocioPremiumWindow
+from src.vista.AdministradorWindow import AdministradorWindow
+from src.modelo.BussinessobjectSocio import BussinessobjectSocio
+from src.controlador.CoordinadorAdmin import CoordinadorAdmin
+from src.modelo.BussinessobjectAdmin import BussinessobjectAdmin
+from src.controlador.CoordinadorEntrenador import CoordinadorEntrenador
+from src.modelo.BussinessobjectEntrenador import BussinessobjectEntrenador
+from src.controlador.CoordinadorSocio import CoordinadorSocio
+
+class CoordinadorPrincipal():
+
+    def __init__(self, ventanaLogin, modelo):
+        self._ventanaLogin = ventanaLogin
+        self._modelo = modelo
+        self._ventanaActual = None
+
+    @property
+    def ventana_login(self):
+        return self._ventanaLogin
+        
+    def iniciarlogin(self, usuario, contrasena):
+        loginVO = LoginVO(usuario, contrasena)
+        usuario = self._modelo.comprobarLogin(loginVO)
+        if not usuario:
+            print("Credenciales incorrectas")
+            return
+
+        self._ventanaLogin.close()
+
+        if usuario[0] == 'administrativo':
+            modelo_admin = BussinessobjectAdmin()
+            self._ventanaActual = AdministrativoWindow()
+            coordinador_admin = CoordinadorAdmin(self._ventanaActual, modelo_admin, self)
+            self._ventanaActual.controlador = coordinador_admin
+            self._ventanaActual.modelo = modelo_admin
+        elif usuario[0] == 'entrenador':
+            modelo_entrenador = BussinessobjectEntrenador()
+            self._ventanaActual = EntrenadorWindow(usuario[1])
+            coordinador_entrenador = CoordinadorEntrenador(self._ventanaActual, modelo_entrenador, self)
+            self._ventanaActual.controlador = coordinador_entrenador
+            self._ventanaActual.modelo = modelo_entrenador
+        elif usuario[0] == 'administrador':
+            modelo_admin = BussinessobjectAdmin()
+            self._ventanaActual = AdministradorWindow()
+            coordinador_admin = CoordinadorAdmin(self._ventanaActual, modelo_admin, self)
+            self._ventanaActual.controlador = coordinador_admin
+            self._ventanaActual.modelo = modelo_admin
+        elif usuario[0] == 'premium':
+            modelo_socio = BussinessobjectSocio()
+            self._ventanaActual = SocioPremiumWindow(usuario[1])
+            coordinador_socio = CoordinadorSocio(self._ventanaActual, modelo_socio, self)
+            self._ventanaActual.controlador = coordinador_socio
+            self._ventanaActual.modelo = modelo_socio
+        else:
+            modelo_socio = BussinessobjectSocio()
+            self._ventanaActual = SocioWindow(usuario[1])
+            coordinador_socio = CoordinadorSocio(self._ventanaActual, modelo_socio, self)
+            self._ventanaActual.controlador = coordinador_socio
+            self._ventanaActual.modelo = modelo_socio
+
+        self._ventanaActual.show()
+
+    def cerrarsesion(self):
+            if self._ventanaActual:
+                self._ventanaActual.close()
+            self._ventanaLogin.resetear()
+            self._ventanaLogin.show()
+            self._ventanaActual = None
+    
+    def actualizarContrasena(self, usuario, contrasena):
+        loginVO = LoginVO(usuario, contrasena)
+        return self._modelo.actualizarContrasena(loginVO)
+
+    def verificarCorreo(self, usuario):
+        return self._modelo.verificarCorreo(usuario)
+
