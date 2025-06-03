6a6f40f0e58cfa045d6ba99ac81cb39970141adb
mrebot00 <147399784+mrebot00@users.noreply.github.com>
2025-06-04 00:29:02 +0200

    Add files via upload

--- src/vista/SocioWindow.py
+++ src/vista/SocioWindow.py
@@ -7,10 +7,11 @@ class SocioWindow(QMainWindow, Form):
     def __init__(self, id_socio):
         super().__init__()
         self.setupUi(self)
+        self.setFixedSize(self.size())
         self.id_socio = id_socio
         self.pushButtonCerrar.clicked.connect(self.cerrar_button_click)
-        self.pushButtonCancelar.clicked.connect(self.confirmar_baja)  # Botón de baja
-        self.pushButtonRenovar.clicked.connect(self.abrir_renovar_suscripcion)
+        self.pushButtonCancelar.clicked.connect(self.confirmar_baja)
+        self.pushButtonRenovar.clicked.connect(self.abrir_renovar_suscripcion)  # Botón de baja
 
     def cerrar_button_click(self):
         print("Cerrando sesión")
@@ -31,9 +32,10 @@ class SocioWindow(QMainWindow, Form):
                 self._controlador.dar_de_baja_socio(self.id_socio)
                 QMessageBox.information(self, "Cuenta eliminada", "Su cuenta ha sido eliminada.")
                 self._controlador.cerrarsesion()
+
     def abrir_renovar_suscripcion(self):
         self._controlador.abrir_renovar_suscripcion(self.id_socio)
-    
+
     @property
     def controlador(self):
         return self._controlador
