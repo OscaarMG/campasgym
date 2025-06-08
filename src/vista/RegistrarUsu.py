fca9ee5e793dc9566376ddb23c65b74ad98822dd
OscaarMG <omartg08@estudiantes.unileon.es>
2025-06-08 17:38:51 +0200

    QMessage

--- src/vista/RegistrarUsu.py
+++ src/vista/RegistrarUsu.py
@@ -22,13 +22,16 @@ class RegistrarUsu(QMainWindow, Form):
         contrasena = self.lineContrasena.text()
         tipo = self.comboBoxTipoUsu.currentText()
 
-        if self.controlador.registrar_usuario(nombre, apellidos, dni, usuario, contrasena, email, tipo):
-            QMessageBox.information(self, "Éxito", "Usuario registrado correctamente.")
+        if not nombre or not apellidos or not email or not dni or not usuario or not contrasena:
+            QMessageBox.warning(self, "Error", "Todos los campos son obligatorios.")
         else:
-            QMessageBox.warning(self, "Error", "No se pudo registrar el usuario.")
+            if self.controlador.registrar_usuario(nombre, apellidos, dni, usuario, contrasena, email, tipo):
+                QMessageBox.information(self, "Éxito", "Usuario registrado correctamente.")
+            else:
+                QMessageBox.warning(self, "Error", "No se pudo registrar el usuario.")
 
-        self.controlador.volver_panel_principal()
-        self.close()
+            self.controlador.volver_panel_principal()
+            self.close()
 
     def volver_panel_principal(self):
         if hasattr(self, "controlador"):
